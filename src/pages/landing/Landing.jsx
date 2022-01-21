51370acf109ca69117cf8af7709a7ce10f6cd69f
Minh Nguyen <3langn@gmail.com>
2022-01-22 02:55:57 +0700

    init

---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,14 @@
+import '../landing/landing.css'
+export default function Landing() {
+  return (
+    <>
+      <div className="container">
+        <div className="logoContainer">
+          <img className="logo" src="./assets/images/logo.png" alt="logo" />
+          <span className="logoName">ISEKAI</span>
+          <span className="logoName2">Minh Nguyên đẹp trai</span>
+        </div>
+      </div>
+    </>
+  );
+}
