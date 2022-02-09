b941855263a0653fa074bad505b93c3aade3d5c6
Minh Nguyen <3langn@gmail.com>
2022-02-09 19:42:50 +0700

    fix input comment

--- src/components/InputComment/InputComment.jsx
+++ src/components/InputComment/InputComment.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { UserImg } from '..';
 import { AiOutlineSend } from 'react-icons/ai';
-import styled from './InputComment.module.scss';
 import { useSelector } from 'react-redux';
+import { UserImg } from '..';
 import { authSelector } from '../../features/authSlice';
+import styled from './InputComment.module.scss';
 const InputComment = ({ onChange, onSendComment, disabledBtn, value, className = '', onKeyDown, showText }) => {
   const { user } = useSelector(authSelector);
   return (
