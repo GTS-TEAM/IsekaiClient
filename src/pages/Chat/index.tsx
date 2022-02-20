005d22fe668e1c54d3e59c8a3917bc39d2e291cd
hi02102002e <2051120240@ut.edu.vn>
2022-02-20 22:02:25 +0700

    socket

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,13 +1,27 @@
 import { Box } from '@mui/material';
 import Layout from 'components/Layout/Layout';
-import React from 'react';
+import { authSelector } from 'features/authSlice';
+import { useAppSelector } from 'hooks/hooks';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
 import ChatMain from './components/ChatMain';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import TypeMessage from './components/TypeMessage';
 import { ChatBody, StyledChat } from './styles';
 
+const END_POINT = 'wss://isekai-api.me/api/socket.io';
+
 const Chat = () => {
+  const { token } = useAppSelector(authSelector);
+  useEffect(() => {
+    const socket = io(END_POINT, {
+      query: {
+        token: token.access_token,
+      },
+      transports: ['websocket'],
+    });
+  }, [token]);
   return (
     <Layout>
       <StyledChat className="layout">
